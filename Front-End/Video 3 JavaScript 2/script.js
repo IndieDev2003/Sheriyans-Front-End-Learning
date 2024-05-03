89e0a5d6a94bc94669b1f8acaa39be565aff910c
Gagan Suman <ilovescience584@gmail.com>
2024-05-03 14:25:26 +0530

    Day 4 Completed

---
+++ Front-End/Video 3 JavaScript 2/script.js
@@ -0,0 +1,11 @@
+  //Learned Diffrence between Var Const And Let
+Learned About Window
+Heap Memory
+Execution Context
+Lexical Order
+How To copy refrence type values
+truthy and falsy
+foreach loop
+forin loop
+do-while loop
+
